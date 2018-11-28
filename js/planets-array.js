75bed96e639f84f4878443bd695f7af90d67663a
Bryan Matta <bryan.m.matta@gmail.com>
2018-11-28 15:41:37 -0600

    finished planets exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -1,16 +1,7 @@
 (function(){
     "use strict";
 
-    var planets = [
-        'Mercury',
-        'Venus',
-        'Earth',
-        'Mars',
-        'Jupiter',
-        'Saturn',
-        'Uranus',
-        'Neptune'
-    ];
+    var planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
 
     /**
      * TODO:
@@ -19,22 +10,30 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift('The Sun');
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push('Pluto');
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift();
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(planets.indexOf('Earth'));
+
 
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
 })();
